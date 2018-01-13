06011ae7e24ebf53e5336257b3fe7ec7ea556447
Augusto Falcao <falcaoaugustos@gmail.com>
2018-01-12 22:21:17 -0300

    feat (example): create example controller with cpf-cnpj text field

--- CPF_CNPJ-Mask/ViewController.swift
+++ CPF_CNPJ-Mask/ViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var CPFCNPJTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        CPFCNPJTextField.becomeFirstResponder()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    // MARK: Text Field Delegate
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == CPFCNPJTextField {
+            if let count = textField.text?.count, count == 18, string.count > 0 {
+                return false
+            }
+
+            guard let text = textField.text else { return false }
+
+            let numberPattern: MaskType = text.count > 15 || (text.count > 13 && string.count > 0)
+                ? .CNPJ : .CPF
+
+            textField.text = string.count < 1 ?
+                CPFCNPJMask.applyMask(numberPattern, toText: String(text.dropLast())) :
+                CPFCNPJMask.applyMask(numberPattern, toText: text + string)
+
+            return false
+        }
+
+        return true
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
